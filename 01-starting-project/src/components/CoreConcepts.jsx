dd33cb8c5ef29c095df9cd391f29ec0417e36ac4
sdawkare <shubham.dawkare@xperi.com>
2025-12-01 22:31:41 +0530

    react study starter project

---
+++ 01-starting-project/src/components/CoreConcepts.jsx
@@ -0,0 +1,13 @@
+import { CORE_CONCEPTS } from "../data";
+import CoreConcept from "./CoreConcept";
+import Section from "./Section";
+
+export default function CoreConcepts() {
+  return (
+    <Section title="Core Concepts" id="core-concepts">
+      <ul>
+        {CORE_CONCEPTS.map(c => <CoreConcept {...c}/>)}
+      </ul>
+  </Section>
+  );
+}
