14e9ff69d377e144a3ad485f2ac36f5a34783b03
sdawkare <shubham.dawkare@xperi.com>
2025-12-16 23:22:37 +0530

    quiz app complete with timers, highlighted answers and summary

---
+++ Quiz app/src/components/Question.jsx
@@ -0,0 +1,68 @@
+
+
+import { useState } from "react";
+import Answers from "./Answers";
+import QuestionTimer from "./QuestionTimer";
+import QUESTIONS from '../questions.js';
+
+export default function Question({ questionIndex, onSelectAnswer, onSkipAnswer }) {
+  
+  const [ answer, setAnswer ] = useState({
+    selectedAnswer: '',
+    isCorrect: null
+  });
+
+  let timer = 10000;
+
+  if (answer.selectedAnswer) {
+    timer = 1000;
+  }
+
+  if (answer.isCorrect !== null) {
+    timer = 2000;
+  }
+
+  function handleSelectAnswer(answer) {
+    setAnswer({
+      selectedAnswer: answer,
+      isCorrect: null,
+    });
+
+    setTimeout(() => {
+      const isCorrect = answer === QUESTIONS[questionIndex].answers[0];
+      setAnswer({
+        selectedAnswer: answer,
+        isCorrect,
+      });
+
+       setTimeout(() => {
+        onSelectAnswer(answer);
+      }, 2000);
+    }, 1000);   
+  }
+
+  let answerState = '';
+  if (answer.selectedAnswer && answer.isCorrect !== null) {
+    answerState = answer.isCorrect ? 'correct' : 'wrong';
+  } else if (answer.selectedAnswer) {
+    answerState = 'answered';
+  }
+
+  return (
+    <div id='question'>
+      <QuestionTimer
+        key={timer}
+        timeout={timer}
+        onTimeOut={answer.selectedAnswer === '' ? onSkipAnswer : null}
+        mode={answerState}
+      />
+      <h2>{QUESTIONS[questionIndex].text}</h2>
+      <Answers
+        answers={QUESTIONS[questionIndex].answers}
+        selectedAnswer={answer.selectedAnswer}
+        answerState={answerState}
+        onSelectAnswer={handleSelectAnswer}
+      />
+    </div>
+  )
+}
---
+++ Quiz app/src/components/QuestionTimer.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+
+export default function QuestionTimer({ timeout, onTimeOut, mode }) {
+  const [remainingTime, setRemainingTime ] = useState(timeout);
+  
+  useEffect(() => {  
+    const timer = setTimeout(onTimeOut, timeout);
+    return () => clearTimeout(timer);
+  }, [timeout, onTimeOut]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime(prevTime => prevTime - 100);
+    }, 100);
+
+    return () => {
+      clearInterval(interval); 
+    };
+  }, []);
+
+  return (
+    <progress id='question-time' value={remainingTime} max={timeout} className={mode}/>
+  );
+}
